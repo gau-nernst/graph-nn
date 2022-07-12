f873b376de289ab054a3ee3ad6a23f13e1bb70ba
Thien Tran <gau.nernst@yahoo.com.sg>
2022-07-12 12:27:28 +0800

    remove bloats from GCN and use CSR tensor

--- gcn.py
+++ gcn.py
@@ -1,41 +1,20 @@
-from functools import partial
-from typing import Optional
-
 import torch
 from torch import nn
 
-from utils import _Activation, _Norm, add_self_loops
+from utils import add_self_loops
 
 
 class GCNLayer(nn.Module):
-    def __init__(
-        self,
-        input_dim: int,
-        output_dim: int,
-        norm: Optional[_Norm] = None,
-        activation: _Activation = partial(nn.ReLU, inplace=True),
-    ):
+    def __init__(self, input_dim: int, output_dim: int):
         super().__init__()
-        self.norm = None if norm is None else norm(input_dim)
         self.linear = nn.Linear(input_dim, output_dim, bias=False)
-        self.act = activation()
 
-    def forward(
-        self,
-        x: torch.Tensor,
-        edge_indices: Optional[torch.Tensor] = None,
-        norm_adj_mat: Optional[torch.Tensor] = None,
-    ):
-        assert (edge_indices is not None) or (norm_adj_mat is not None)
-        if norm_adj_mat is None:
-            norm_adj_mat = self.create_norm_adj_mat(edge_indices, x.shape[0])
-        if self.norm is not None:
-            x = self.norm(x)
-        return self.act(norm_adj_mat @ self.linear(x))
+    def forward(self, x: torch.Tensor, adj_mat: torch.Tensor) -> torch.Tensor:
+        return adj_mat @ self.linear(x)
 
     @staticmethod
-    def create_norm_adj_mat(edge_indices: torch.Tensor, num_nodes: int):
+    def normalize_adj_mat(edge_indices: torch.Tensor, num_nodes: int) -> torch.Tensor:
         edge_indices = add_self_loops(edge_indices=edge_indices, num_nodes=num_nodes)
         deg_rsqrt = torch.bincount(edge_indices[0], minlength=num_nodes).rsqrt()
         values = deg_rsqrt[edge_indices[0]] * deg_rsqrt[edge_indices[1]]
-        return torch.sparse_coo_tensor(edge_indices, values).coalesce()
+        return torch.sparse_coo_tensor(edge_indices, values).to_sparse_csr()
