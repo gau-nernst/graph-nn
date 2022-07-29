0b0feece0bab9cfd49609f6107a8e910c76f2d58
Thien Tran <gau.nernst@yahoo.com.sg>
2022-07-29 22:47:13 +0800

    add multi-head

--- graph_nn/gt.py
+++ graph_nn/gt.py
@@ -21,7 +21,9 @@ class GTLayer(nn.Module):
         if feedforward_dim is None:
             feedforward_dim = embed_dim * 4
         super().__init__()
-        self.linear = nn.Linear(embed_dim, embed_dim * 3)
+        self.attn_linear = nn.Linear(embed_dim, embed_dim * 3)
+        self.head_dim = embed_dim // num_heads
+        self.num_heads = num_heads
         self.scale = embed_dim ** (-0.5)
         self.ffn = nn.Sequential(
             nn.Linear(embed_dim, feedforward_dim),
@@ -46,8 +48,15 @@ class GTLayer(nn.Module):
     def _self_attention(
         self, x: torch.Tensor, edge_indices: torch.Tensor
     ) -> torch.Tensor:
-        q, k, v = self.linear(x).chunk(3, dim=1)
-        alpha = (q[edge_indices[0]] * k[edge_indices[1]]).sum(1) * self.scale
+        qkv = self.attn_linear(x).reshape(-1, self.num_heads, self.head_dim * 3)
+        q, k, v = qkv.chunk(3, dim=2)
+        alpha = (q[edge_indices[0]] * k[edge_indices[1]]).sum(2) * self.scale
         alpha_mat = torch.sparse_coo_tensor(edge_indices, alpha, requires_grad=True)
-        weights = torch.sparse.softmax(alpha_mat, dim=1)
-        return self.dropout(torch.sparse.mm(weights, v))
+        weights = torch.sparse.softmax(alpha_mat, dim=1).coalesce()
+        indices = weights.indices()
+        values = weights.values()
+        out = []
+        for i in range(self.num_heads):
+            w_i = torch.sparse_coo_tensor(indices, values[:, i])
+            out.append(torch.sparse.mm(w_i, v[:, i]))
+        return self.dropout(torch.cat(out, dim=1))
