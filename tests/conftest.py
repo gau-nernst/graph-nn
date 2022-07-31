304849360d00d7a772246ec79d1126ddb1e609d3
Thien Tran <gau.nernst@yahoo.com.sg>
2022-07-31 16:11:13 +0800

    add tests for GAT

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,7 @@ def num_nodes():
     return 100
 
 
-@pytest.fixture(scope="module")
-def symmetric_adj_mat_idx(num_nodes: int):
-    density = 0.1
+def _adj_mat_idx(num_nodes: int, density: float, symmetric: bool):
     nnz = int(num_nodes * num_nodes * density)
 
     indices = set()
@@ -20,7 +18,20 @@ def symmetric_adj_mat_idx(num_nodes: int):
         if i == j:
             continue
         indices.add((i, j))
-        indices.add((j, i))
+        if symmetric:
+            indices.add((j, i))
 
     indices = torch.tensor(sorted(indices)).t()
     return indices
+
+
+@pytest.fixture(scope="module")
+def symmetric_adj_mat_idx(num_nodes: int):
+    density = 0.1
+    return _adj_mat_idx(num_nodes, density, True)
+
+
+@pytest.fixture(scope="module")
+def asymmetric_adj_mat_idx(num_nodes: int):
+    density = 0.1
+    return _adj_mat_idx(num_nodes, density, False)
